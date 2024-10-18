a645b7f8baf087f93088d6c08f8cc9b57a0546ce
Nauris Linde <naurislinde@gmail.com>
2024-10-18 20:01:08 +0000

    style: format cmake files

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,14 +1,27 @@
 function(setup_dependencies)
-    find_package(QT NAMES Qt6 REQUIRED COMPONENTS Core)
+  find_package(
+    QT
+    NAMES
+    Qt6
+    REQUIRED
+    COMPONENTS Core)
 
-    if(USE_SENTRY AND NOT TARGET sentry::sentry)
-        CPMAddPackage(
-            NAME sentry
-            VERSION 0.7.6
-            GITHUB_REPOSITORY getsentry/sentry-native
-            GIT_TAG "0.7.6"
-            OPTIONS "SENTRY_BACKEND breakpad" "SENTRY_INTEGRATION_QT ON" "SENTRY_BUILD_TESTS OFF" "SENTRY_BUILD_EXAMPLES OFF"
-            SYSTEM YES
-        )
-    endif()
+  if(USE_SENTRY AND NOT TARGET sentry::sentry)
+    cpmaddpackage(
+      NAME
+      sentry
+      VERSION
+      0.7.6
+      GITHUB_REPOSITORY
+      getsentry/sentry-native
+      GIT_TAG
+      "0.7.6"
+      OPTIONS
+      "SENTRY_BACKEND breakpad"
+      "SENTRY_INTEGRATION_QT ON"
+      "SENTRY_BUILD_TESTS OFF"
+      "SENTRY_BUILD_EXAMPLES OFF"
+      SYSTEM
+      YES)
+  endif()
 endfunction()
