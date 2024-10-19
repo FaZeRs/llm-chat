5e394deb76c1879f453a14ae74076fb84c0b3237
Nauris Linde <naurislinde@gmail.com>
2024-10-19 16:42:23 +0000

    test: fix chat thread tests

--- tests/tst_chat_thread.cpp
+++ tests/tst_chat_thread.cpp
@@ -8,58 +8,47 @@ class TestChatThread : public QObject {
  private slots:
   void testAddMessage() const;
   void testUpdateMessageText() const;
-  void testUpdateMessageContext() const;
   void testClearMessages() const;
 };
 
 void TestChatThread::testAddMessage() const {
-  llm_chat::ChatThread thread;
+  llm_chat::Thread thread;
   QCOMPARE(thread.rowCount(), 0);
 
   thread.addMessage("Hello", true, {});
   QCOMPARE(thread.rowCount(), 1);
-  QCOMPARE(thread.data(thread.index(0, 0), llm_chat::ChatThread::TextRole)
-               .toString(),
-           "Hello");
-  QCOMPARE(thread.data(thread.index(0, 0), llm_chat::ChatThread::IsUserRole)
-               .toBool(),
-           true);
+  QCOMPARE(
+      thread.data(thread.index(0, 0), llm_chat::Thread::TextRole).toString(),
+      "Hello");
+  QCOMPARE(
+      thread.data(thread.index(0, 0), llm_chat::Thread::IsUserRole).toBool(),
+      true);
 
   thread.addMessage("World", false, {QVariant("context")});
   QCOMPARE(thread.rowCount(), 2);
-  QCOMPARE(thread.data(thread.index(1, 0), llm_chat::ChatThread::TextRole)
-               .toString(),
-           "World");
-  QCOMPARE(thread.data(thread.index(1, 0), llm_chat::ChatThread::IsUserRole)
-               .toBool(),
-           false);
-  QCOMPARE(thread.data(thread.index(1, 0), llm_chat::ChatThread::ContextRole)
-               .toList(),
-           QVariantList({"context"}));
+  QCOMPARE(
+      thread.data(thread.index(1, 0), llm_chat::Thread::TextRole).toString(),
+      "World");
+  QCOMPARE(
+      thread.data(thread.index(1, 0), llm_chat::Thread::IsUserRole).toBool(),
+      false);
+  QCOMPARE(
+      thread.data(thread.index(1, 0), llm_chat::Thread::ContextRole).toList(),
+      QVariantList({"context"}));
 }
 
 void TestChatThread::testUpdateMessageText() const {
-  llm_chat::ChatThread thread;
+  llm_chat::Thread thread;
   thread.addMessage("Hello", true, {});
 
-  thread.updateMessageText(0, " World");
-  QCOMPARE(thread.data(thread.index(0, 0), llm_chat::ChatThread::TextRole)
-               .toString(),
-           "Hello World");
-}
-
-void TestChatThread::testUpdateMessageContext() const {
-  llm_chat::ChatThread thread;
-  thread.addMessage("Hello", true, {});
-
-  thread.updateMessageContext(0, {QVariant("context")});
-  QCOMPARE(thread.data(thread.index(0, 0), llm_chat::ChatThread::ContextRole)
-               .toList(),
-           QVariantList({"context"}));
+  thread.updateLatestMessage(" World");
+  QCOMPARE(
+      thread.data(thread.index(0, 0), llm_chat::Thread::TextRole).toString(),
+      "Hello World");
 }
 
 void TestChatThread::testClearMessages() const {
-  llm_chat::ChatThread thread;
+  llm_chat::Thread thread;
   thread.addMessage("Hello", true, {});
   thread.addMessage("World", false, {});
   QCOMPARE(thread.rowCount(), 2);
